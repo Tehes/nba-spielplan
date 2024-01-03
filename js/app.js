dddcd932fabe1a7b562bc1bb1098d23f29ecdee0
Tino Sabetta <tino.sabetta@gmail.com>
2024-01-03 17:54:04 +0100

    added skeleton screens and scores for finished games

--- js/app.js
+++ js/app.js
@@ -43,64 +43,81 @@ Variables
 const data = await fetchSchedule("2023");
 const games = [];
 const template = document.querySelector("template");
-const mainEl = document.querySelector("main");
+const todayEl = document.querySelector("#today");
 const today = new Date();
 
 /* --------------------------------------------------------------------------------------------------
 functions
 ---------------------------------------------------------------------------------------------------*/
-data.lscd.forEach(months => {
-    months.mscd.g.forEach(game => {
-        game.localDate = new Date(Date.parse(game.gdtutc + "T" + game.utctm + "+00:00"));
+function prepareGameData() {
+    data.lscd.forEach(months => {
+        months.mscd.g.forEach(game => {
+            game.localDate = new Date(Date.parse(game.gdtutc + "T" + game.utctm + "+00:00"));
 
-        let year = game.localDate.getFullYear();
-        let month = game.localDate.getMonth() + 1;
-        let day = game.localDate.getDate();
-        game.hours = game.localDate.getHours().toString().padStart(2, '0');
-        game.minutes = game.localDate.getMinutes().toString().padStart(2, '0');
+            let year = game.localDate.getFullYear();
+            let month = game.localDate.getMonth() + 1;
+            let day = game.localDate.getDate();
+            game.hours = game.localDate.getHours().toString().padStart(2, '0');
+            game.minutes = game.localDate.getMinutes().toString().padStart(2, '0');
 
-        game.dateString = `${day}.${month}.${year} - ${game.hours}:${game.minutes} Uhr`;
+            game.dateString = `${day}.${month}.${year} - ${game.hours}:${game.minutes} Uhr`;
 
-        games.push(game);
+            games.push(game);
+        });
     });
-});
 
-function compareDate(a, b) {
-    return a.localDate - b.localDate;
+    function compareDate(a, b) {
+        return a.localDate - b.localDate;
+    }
+
+    games.sort(compareDate);
 }
 
-games.sort(compareDate);
+function renderTodaysGames() {
+    todayEl.innerHTML = "";
+    games.forEach(g => {
+        if (today.toLocaleDateString("de-DE") == g.localDate.toLocaleDateString("de-DE")) {
+            console.log(g.stt)
 
-games.forEach(g => {
-    if (today.toLocaleDateString("de-DE") == g.localDate.toLocaleDateString("de-DE")) {
-        const clone = template.content.cloneNode(true);
+            const clone = template.content.cloneNode(true);
 
-        const homeTeam = clone.querySelector(".home-team");
-        const visitingTeam = clone.querySelector(".visiting-team");
-        const homeLogo = clone.querySelectorAll("img")[1];
-        const homeAbbr = clone.querySelector(".h-abbr");
-        const visitingAbbr = clone.querySelector(".v-abbr");
-        const visitingLogo = clone.querySelectorAll("img")[0];
-        const homeName = clone.querySelector(".h-name");
-        const visitingName = clone.querySelector(".v-name");
-        const date = clone.querySelector(".date");
+            const homeTeam = clone.querySelector(".home-team");
+            const visitingTeam = clone.querySelector(".visiting-team");
+            const homeLogo = clone.querySelectorAll("img")[1];
+            const homeAbbr = clone.querySelector(".h-abbr");
+            const visitingAbbr = clone.querySelector(".v-abbr");
+            const visitingLogo = clone.querySelectorAll("img")[0];
+            const homeName = clone.querySelector(".h-name");
+            const visitingName = clone.querySelector(".v-name");
+            const date = clone.querySelector(".date");
 
-        homeTeam.style.setProperty("background-color", `var(--${g.h.ta})`);
-        visitingTeam.style.setProperty("background-color", `var(--${g.v.ta})`);
-        homeLogo.src = `img/${g.h.ta}.svg`;
-        visitingLogo.src = `img/${g.v.ta}.svg`;
-        homeName.textContent = `${g.h.tc} ${g.h.tn}`;
-        visitingName.textContent = `${g.v.tc} ${g.v.tn}`;
-        homeAbbr.textContent = g.h.ta;
-        visitingAbbr.textContent = g.v.ta;
-        date.textContent = `${g.hours}:${g.minutes} Uhr`;
+            homeTeam.style.setProperty("background-color", `var(--${g.h.ta})`);
+            visitingTeam.style.setProperty("background-color", `var(--${g.v.ta})`);
+            homeLogo.src = `img/${g.h.ta}.svg`;
+            visitingLogo.src = `img/${g.v.ta}.svg`;
+            homeName.textContent = `${g.h.tc} ${g.h.tn}`;
+            visitingName.textContent = `${g.v.tc} ${g.v.tn}`;
+            homeAbbr.textContent = g.h.ta;
+            visitingAbbr.textContent = g.v.ta;
+            
+            if (g.stt === "Final") {
+                date.textContent = `${g.v.s}:${g.h.s}`;
+            }
+            else {
+                date.textContent = `${g.hours}:${g.minutes} Uhr`;
+            }
+            
 
-        mainEl.appendChild(clone);
-    }
-});
+            todayEl.appendChild(clone);
+        }
+    });
+}
+
 
 function init() {
     document.addEventListener("touchstart", function () { }, false);
+    prepareGameData();
+    renderTodaysGames();
 }
 
 /* --------------------------------------------------------------------------------------------------
