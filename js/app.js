06a620f09f7fb498bf55c4b1975077f80920a097
Tino Sabetta <tino.sabetta@gmail.com>
2025-12-11 18:03:45 +0100

    Improve playoffs and cup layout

--- js/app.js
+++ js/app.js
@@ -1752,7 +1752,7 @@ globalThis.app.init();
  * - AUTO_RELOAD_ON_SW_UPDATE: reload page once after an update
  -------------------------------------------------------------------------------------------------- */
 const USE_SERVICE_WORKER = true;
-const SERVICE_WORKER_VERSION = "2025-12-11-v2";
+const SERVICE_WORKER_VERSION = "2025-12-11-v3";
 const AUTO_RELOAD_ON_SW_UPDATE = true;
 
 /* --------------------------------------------------------------------------------------------------
